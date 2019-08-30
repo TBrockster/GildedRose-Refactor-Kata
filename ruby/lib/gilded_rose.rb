00b841235a06345d04b04a8c00b3941188594547
TBrockster <tobybrock93@gmail.com>
2019-08-30 14:26:58 +0100

    extracted out update_sell_in method

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
 class GildedRose
+  SELL_IN_EXCEPTIONS = ['Sulfuras, Hand of Ragnaros']
   def initialize(items)
     @items = items
   end
 
   def update_quality
     @items.each do |item|
-      item.sell_in -= 1
+      update_sell_in(item)
       case item.name
       when 'Sulfuras, Hand of Ragnaros'
-        item.sell_in += 1
+        # nothing effects Sulfuras, Hand of Ragnaros!
       when 'Backstage passes to a TAFKAL80ETC concert'
         case item.sell_in
         when 10..(1.0 / 0)
@@ -29,6 +30,10 @@ class GildedRose
         item.quality -= 1 if item.quality > 0 && item.sell_in < 0
       end
     end
+  end
+
+  def update_sell_in(item)
+    item.sell_in -= 1 unless SELL_IN_EXCEPTIONS.include? item.name
   end
 end
 
