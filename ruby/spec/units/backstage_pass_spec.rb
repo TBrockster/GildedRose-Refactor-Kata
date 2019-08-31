5fedad1e675f004d2c0a629c839bf1d2de3f2755
TBrockster <tobybrock93@gmail.com>
2019-09-01 00:57:17 +0100

    begun writing backstage pass #update quality unit tests

--- ruby/spec/units/backstage_pass_spec.rb
+++ ruby/spec/units/backstage_pass_spec.rb
@@ -4,24 +4,26 @@ require 'backstage_pass'
 
 describe BackstagePass do
   before(:each) do
-    @backstage_pass = BackstagePass.new('Backstage passes to a TAFKAL80ETC concert', 10, 10)
+    @expired_pass = BackstagePass.new('Backstage passes to a TAFKAL80ETC concert', 0, 10)
+    @one_day_pass = BackstagePass.new('Backstage passes to a TAFKAL80ETC concert', 1, 10)
+    @five_day_pass = BackstagePass.new('Backstage passes to a TAFKAL80ETC concert', 5, 10)
+    @six_day_pass = BackstagePass.new('Backstage passes to a TAFKAL80ETC concert', 6, 10)
+    @ten_day_pass = BackstagePass.new('Backstage passes to a TAFKAL80ETC concert', 10, 10)
+    @eleven_day_pass = BackstagePass.new('Backstage passes to a TAFKAL80ETC concert', 11, 10)
   end
 
   describe '#update_sell_in:' do
     it 'Reduces sell_in by 1.' do
-      @backstage_pass.update_sell_in
-      expect(@backstage_pass.sell_in).to eq 9
+      @expired_pass.update_sell_in
+      expect(@expired_pass.sell_in).to eq(-1)
     end
   end
 
   describe '#update_quality' do
-    # it 'Increases quality by 1.' do
-    #   @aged_brie.update_quality
-    #   expect(@aged_brie.quality).to eq 1
-    # end
-    # it 'Does not raise quality above 50.' do
-    #   @high_quality_brie.update_quality
-    #   expect(@high_quality_brie.quality).to eq 50
-    # end
+    it 'Reduces pass quality to 0 when sell_in is less than 0.' do
+      @expired_pass.update_sell_in
+      @expired_pass.update_quality
+      expect(@expired_pass.quality).to eq 0
+    end
   end
 end
