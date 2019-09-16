8a056fb686356d3c1dcee8240bf1626cd4484b32
TBrockster <tobybrock93@gmail.com>
2019-09-16 15:57:03 +0100

    Dried out update_helper.rb

--- ruby/spec/units/update_helper.rb
+++ ruby/spec/units/update_helper.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+def update_inventory(store)
+  store.update_inventory
+end
+
 def update_and_expect_quality(item, expected_result)
   item.update_sell_in
   item.update_quality
@@ -7,11 +11,11 @@ def update_and_expect_quality(item, expected_result)
 end
 
 def update_inventory_and_expect_sell_in(item, expected_result)
-  GildedRose.new(item).update_inventory
+  update_inventory(GildedRose.new(item))
   expect(item[0].sell_in).to eq(expected_result)
 end
 
 def update_inventory_and_expect_quality(item, expected_result)
-  GildedRose.new(item).update_inventory
+  update_inventory(GildedRose.new(item))
   expect(item[0].quality).to eq(expected_result)
 end
