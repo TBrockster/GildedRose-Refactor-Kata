a27f667be916a1771ea9e81b933981db7a795b12
TBrockster <tobybrock93@gmail.com>
2019-09-16 16:04:56 +0100

    refactored aged brie sell in

--- ruby/spec/units/aged_brie_spec.rb
+++ ruby/spec/units/aged_brie_spec.rb
@@ -10,8 +10,7 @@ describe AgedBrie do
 
   describe '#update_sell_in:' do
     it 'Reduces sell_in by 1.' do
-      @aged_brie.update_sell_in
-      expect(@aged_brie.sell_in).to eq 9
+      update_and_expect_sell_in(@aged_brie, 9)
     end
   end
 
--- ruby/spec/units/update_helper.rb
+++ ruby/spec/units/update_helper.rb
@@ -10,6 +10,11 @@ def update_and_expect_quality(item, expected_result)
   expect(item.quality).to eq(expected_result)
 end
 
+def update_and_expect_sell_in(item, expected_result)
+  item.update_sell_in
+  expect(item.sell_in).to eq(expected_result)
+end
+
 def update_inventory_and_expect_sell_in(item, expected_result)
   update_inventory(GildedRose.new(item))
   expect(item[0].sell_in).to eq(expected_result)
